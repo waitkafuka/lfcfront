52d9d1751aa20cb6f92d2335baf9beea7ab7f8e3
高如雪 <xue824340575@outlook.com>
2016-12-06 09:38:33 +0800

    修改

--- src/main/webapp/dev/js/addpost-tag.js
+++ src/main/webapp/dev/js/addpost-tag.js
@@ -16,7 +16,7 @@ $(function() {
 		success: function(data) {
 			if(data) {
 				$(".news_right_list  dl").slice(0,1).html("");
-				var value = data.tagList;
+				 var value = data.tagList;
 				for(var i = 0; i < value.length; i++) {
 					//动态生成a标签，根据接口文档的需要
 					var oEle = $("<dd><a href='#" + value[i].tagId + "'>" + value[i].tagName + "</a></dd>");
