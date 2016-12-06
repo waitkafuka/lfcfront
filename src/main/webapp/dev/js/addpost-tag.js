d9ad93448967d49ad6339aaf65a7730f0a864180
高如雪 <xue824340575@outlook.com>
2016-12-06 09:05:11 +0800

    修改

---
+++ src/main/webapp/dev/js/addpost-tag.js
@@ -0,0 +1,29 @@
+$(function() {
+	Mock.mock("http://localhost:8080/lfcfront/tagList", {
+		"totalCount": 1000,
+		"tagList|20": [{
+			"tagName": "@cname",
+			"tagId|198-218": 20
+		}]
+
+	});
+	$.ajax({
+		type: "get",
+		url: "http://localhost:8080/lfcfront/tagList",
+		async: true,
+		dataType: 'json',
+		data: {},
+		success: function(data) {
+			if(data) {
+				$(".news_right_list  dl").slice(0,1).html("");
+				var value = data.tagList;
+				for(var i = 0; i < value.length; i++) {
+					//动态生成a标签，根据接口文档的需要
+					var oEle = $("<dd><a href='#" + value[i].tagId + "'>" + value[i].tagName + "</a></dd>");
+					$(".news_right_list dl").slice(0, 1).append(oEle);	
+				}
+
+			}
+		}
+	})
+});
