526feb877725f8cd498bff7ea8bae2cc96f50b3f
yaohaohao-xinqing <2416648104@qq.com>
2016-12-08 11:23:40 +0800

    添加验证码功能
    添加验证码：pom添加依赖，web.xml添加servlet映射，添加ValidateCodeServlet.java

--- src/main/java/com/lfcfront/knowledge/controller/user/UserController.java
+++ src/main/java/com/lfcfront/knowledge/controller/user/UserController.java
@@ -1,4 +1,4 @@
-package com.lfcfront.knowledge.controller.user;
+﻿package com.lfcfront.knowledge.controller.user;
 
 import java.awt.Menu;
 import java.util.HashMap;
@@ -109,19 +109,21 @@ public class UserController {
 	@RequestMapping(value = "regist")
 	public Map<String, Object> regist(HttpServletRequest request,
 			String username, String password,String code,HttpSession session) {
+		String sessionCode="";
+		sessionCode=(String)session.getAttribute("validateCode");
 		User user = userService.queryUserByUsername(username);
 		Map<String, Object> result = new HashMap<String, Object>();
 		String msg = null;
 		int state = 10;
 		if (username != null && password != null && code != null) {
-			if (user == null && "3321".equals(code)) {
+			if (user == null && sessionCode=.equals(code)) {
 				state = 0;
 				msg = "注册成功！";
 				User newuser = new User();
 				newuser.setUsername(username);
 				newuser.setPassword(password);
 				userService.saveUser(newuser);
-			} else if (user == null && !"3321".equals(code)) {
+			} else if (user == null && !sessionCode=.equals(code)) {
 				state = 2;
 				msg = "验证码错误！";
 			} else if (user != null) {
